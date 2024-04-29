3e8c4303dc1bdede631e76dc7a5c075840956761
Darien Chen <darien7557@fakeemail.com>
2024-04-29 19:59:12 +0800

    css修改

--- client/src/components/home-component.js
+++ client/src/components/home-component.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import event from "../pictures/event.png";
 
 const HomeComponent = () => {
   return (
@@ -6,37 +8,50 @@ const HomeComponent = () => {
       <div className="container py-4">
         <div className="p-5 mb-4 bg-warning rounded-3">
           <div className="container-fluid py-5">
-            <h1 className="display-5 fw-bold">劇本殺預約排程系統</h1>
-            <p className="col-md-8 fs-4">
+            <h1 className="display-2 fw-bold">劇本殺預約排程系統</h1>
+            <p className="col-md-8 fs-2">
               本系統是一個提供全台劇本殺活動之揪團、參團、預約等交流平台.玩家及主持人們可透過此平台進行交流、互動.
             </p>
             <button className="btn btn-primary btn-lg" type="button">
-              看看它怎麼運作。
+              了解劇本殺
             </button>
           </div>
         </div>
 
         <div className="row align-items-md-stretch">
-          <div className="col-md-6">
+          <div className="col-md-6 mb-2">
             <div className="h-100 p-5 text-white bg-dark rounded-3">
               <h2>作為一個玩家</h2>
-              <p>
-                玩家可以參加他們喜歡的劇本殺團。
-              </p>
-              <button className="btn btn-primary" type="button">
-                登錄會員、或者註冊一個帳號
-              </button>
+              <p>玩家可以參加他們喜歡的劇本殺團。</p>
+              <Link
+                style={{ margin: "0.5rem" }}
+                className="btn btn-primary"
+                type="button"
+                to="/register"
+              >
+                註冊帳號
+              </Link>
+              <Link
+                style={{ margin: "0.5rem" }}
+                className="btn btn-primary"
+                type="button"
+                to="/login"
+              >
+                會員登入
+              </Link>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-6 mb-2">
             <div className="h-100 p-5 bg-light border rounded-3">
               <h2>作為一個主持人</h2>
-              <p>
-                您可以通過註冊成為一名主持人，並開始發布您的開團資訊。
-              </p>
-              <button className="btn btn-primary" type="button">
+              <p>您可以通過註冊成為一名主持人，並開始發布您的開團資訊。</p>
+              <Link
+                className="btn btn-primary"
+                type="button"
+                to="/postlarpevent"
+              >
                 揪團去~
-              </button>
+              </Link>
             </div>
           </div>
         </div>
